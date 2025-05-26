7e56002ae27cf8cc778fea9b16fc5a9850858384
Vishnuvichu25 <vishnu25narayanan@gmail.com>
2025-05-26 13:32:27 +0530

    comited8
    updated html and css

--- Scripts_js/cricularprogress.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const progressBars = document.querySelectorAll('.circular-progress');
-
-    progressBars.forEach(bar => {
-        const progress = bar.getAttribute('data-progress');
-        bar.style.setProperty('--progress', progress * 3.6 + 'deg');
-    });
-});
